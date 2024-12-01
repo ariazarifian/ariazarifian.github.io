2fd1e2af73b3fd3b0d53e374372c1924f5cfdb95
Aria Zarifian <azarifian.prestataire@caisses-sociales.mc>
2024-12-01 02:01:10 +0100

    Deployed 6e8e35263 with MkDocs version: 1.6.1

--- assets/parallax.js
+++ assets/parallax.js
@@ -1,22 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const cards = document.querySelectorAll('.card');
-  
-    const handleScroll = () => {
-      const triggerHeight = window.innerHeight / 1.3;
-  
-      cards.forEach((card, index) => {
-        const cardTop = card.getBoundingClientRect().top;
-  
-        if (cardTop < triggerHeight) {
-          setTimeout(() => {
-            card.classList.add('show');
-          }, index * 200);
-        } else {
-          card.classList.remove('show');
-        }
-      });
-    };
-  
-    window.addEventListener('scroll', handleScroll);
-  });
-  
+  const cards = document.querySelectorAll('.card');
+
+  const handleScroll = () => {
+    const triggerHeight = window.innerHeight / 1.3;
+
+    cards.forEach((card, index) => {
+      const cardTop = card.getBoundingClientRect().top;
+
+      if (cardTop < triggerHeight) {
+        setTimeout(() => {
+          card.classList.add('show');
+        }, index * 200);
+      } else {
+        card.classList.remove('show');
+      }
+    });
+  };
+
+  window.addEventListener('scroll', handleScroll);
+});
